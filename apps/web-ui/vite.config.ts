99eb70230397e10e5cdc4dbdde3f8af9d93e53da
frontdev-corp8ai <sachinpal@Sachins-MacBook-Pro.local>
2025-12-23 17:39:23 +0530

    Integrate react web-ui with nginx

--- apps/web-ui/vite.config.ts
+++ apps/web-ui/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Needed for Docker
+    port: 3000, // Standardize port
+    watch: {
+      usePolling: true, // Fixes hot-reload issues in Docker on some systems
+    },
+  },
+});
